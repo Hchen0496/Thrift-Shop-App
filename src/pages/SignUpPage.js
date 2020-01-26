bdcc3c181c33d9d7db643d246e37c39c978ed50b
David Oppong <doppong791@outlook.com>
2020-01-26 00:55:36 -0500

    Update on Sign Up Page. Need to dev REST API to store creds

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,9 +1,61 @@
 import React from 'react'; 
 import '../App.css';
 
+const PASSWORD_LENGTH = 8;
 
 class SignUpPage extends React.Component {
+
+    state = {
+        error: false,
+        errorMessage: '',
+        success: false,
+        firstName: '',
+        lastName: '',
+        email: '',
+        firstPassword: '',
+        secondPassword: ''
+    }
+
+    fieldChanged = (name) => {
+        return (event) => {
+            let {value} = event.target;
+            this.setState({[name]: value});
+        }
+    }
+
+    verifyUserCreds = () => {
+        if (
+            this.state.firstName === '' ||
+            this.state.lastName === '' || 
+            this.state.email === '' || 
+            this.state.firstPassword === '' || 
+            this.state.secondPassword === ''
+        ) {
+            this.setState({
+                error: true,
+                errorMessage: 'Field is required'
+            });
+            return false;
+        }
+
+        if (this.state.firstPassword.length < PASSWORD_LENGTH) {
+            this.setState({
+                error: true,
+                errorMessage: 'Password is short. Must be at least 8 characters'
+            });
+            return false;
+        }
+        return true;
+    }
+    //Function to allow user to sign up and account will be made. 
+    /*signUpUser = (event) => {
+        if(this.verifyUserCreds()) {
+
+        }
+    }*/
+
     render() {
+        if()
         return (
             <div className="bg-sign-up"> {/*Implement first background image*/}
                 <div className="form-inline">    
@@ -16,6 +68,7 @@ class SignUpPage extends React.Component {
                                 type="text"
                                 className="form-control"
                                 placeholder="First Name"
+                                onChange={this.fieldChanged('firstName')}
                             />
                         </div>
                         <div className="col-lg-12">
@@ -23,6 +76,7 @@ class SignUpPage extends React.Component {
                                 type="text"
                                 className="form-control"
                                 placeholder="Last Name"
+                                onChange={this.fieldChanged('lastName')}
                             />
                         </div>
                         <div className="col-lg-12">
@@ -30,6 +84,7 @@ class SignUpPage extends React.Component {
                                 type="text"
                                 className="form-control"
                                 placeholder="Email"
+                                onChange={this.fieldChanged('email')}
                             />
                         </div>
                         <div className="col-lg-12">
@@ -37,6 +92,7 @@ class SignUpPage extends React.Component {
                                 type="text"
                                 className="form-control"
                                 placeholder="password"
+                                onChange={this.fieldChanged('firstPassword')} 
                             />
                         </div>
                         <div className="col-lg-12">
@@ -44,8 +100,8 @@ class SignUpPage extends React.Component {
                                 type="text"
                                 className="form-control"
                                 placeholder="Re-enter password"
+                                onChange={this.fieldChanged('secondPassword')}
                             />
-                            
                         </div>
                         <div className="col-lg-12">
                         <button type="button" class="btn btn-primary">Submit</button>
