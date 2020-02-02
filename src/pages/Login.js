dc54646e41638393eb55e3dad1b6e8beaf8f22f3
Huilin Chen <Huilin.chen24@gmail.com>
2020-02-02 00:14:34 -0500

    Working towards the backend at the moment...

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,23 +4,47 @@ import "../App.css";
 
 export default class Login extends Component {
   state = {
-    email: "",
-    password: "",
+    emails: [],
+    password: [],
     error: null
   };
 
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    fetch("http://Localhost:8080/email")
+      .then(response => response.json())
+      .then(data => this.setState({ emails: data, password: data }));
+  }
+
+  Errormsg(event) {
+    alert("404, cannot Load the site.");
+    alert("Please try again later");
+    event.preventDefault();
+  }
+
   render() {
+    const { emails, password } = this.state;
     return (
       <div className="bg-login">
         <div className="box-for-login">
           <div className="input-style">
-            <input type="email" class="email" placeholder="Email" required />
+            <h1>
+              Welcome To <b>Frugal Fashion!</b>
+            </h1>
+            <input
+              type="email"
+              class="email"
+              placeholder="Email"
+              required
+              onChange={this.state.emails}
+            />
             <br />
             <input
               type="password"
               class="passwords"
               placeholder="Password"
               required
+              onChange={this.state.password}
             />
             <br />
             <button type="submit">Login</button>
